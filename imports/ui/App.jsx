5046f5af593cee07d3bf869597617953739904e7
WAGN DONG <kelvinson@foxmail.com>
2017-07-03 16:40:24 +0800

    add player list and render the list using list and Divider from material-ui.com

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,12 +1,63 @@
 import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
-import AppBar from 'material-ui/AppBar'
+import AppBar from 'material-ui/AppBar';
+import {List } from 'material-ui/List';
+import Divider from 'material-ui/Divider';
+
+
 import Player from "./Player";
 import TeamList from './Team-list'
 import TeamStats from './Team-stats'
 
 export default class App extends Component {
+  getPlayers() {
+    return [
+      {
+        _id: 1,
+        name: "Wang Dong",
+        ballManipulaition:4,
+        kickingAbilities:4,
+        passingAbilities:4,
+        dualTackling:3,
+        fieldCoverage:3,
+        blockingAbilities:4,
+        gameStrategy:4,
+        playmakingRisks:4,
+      },
+      {
+        _id: 2,
+        name: "Speedy Gonz",
+        ballManipulaition:4,
+        kickingAbilities:4,
+        passingAbilities:4,
+        dualTackling:3,
+        fieldCoverage:3,
+        blockingAbilities:4,
+        gameStrategy:4,
+        playmakingRisks:4,
+      },
+      {
+        _id: 3,
+        name: "Tracey Good",
+        ballManipulaition:4,
+        kickingAbilities:4,
+        passingAbilities:4,
+        dualTackling:3,
+        fieldCoverage:3,
+        blockingAbilities:4,
+        gameStrategy:4,
+        playmakingRisks:4,
+      }
+    ];
+  }
+
+  renderPlayers() {
+    return this.getPlayers().map((player) => (
+      <TeamList key={player._id} player={player}/>
+    ));
+  }
+
   render() {
     return (
       <MuiThemeProvider>
@@ -17,8 +68,14 @@ export default class App extends Component {
             showMenuIconButton={false}/>
           <div className="row">
             <div className="col s12 m7" ><Player /></div>
+            <div className="col s12 m5" >
+              <Divider/>
+              <List>
+                {this.renderPlayers()}
+              </List>
+              <Divider/>
+            </div>
             <div className="col s12 m5" ><TeamStats /></div>
-            <div className="col s12 m5" ><TeamList /></div>
           </div>
         </div>
       </MuiThemeProvider>
