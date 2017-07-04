4f6f0ef99fab825f42a5eebaed80c318ac90daea
WAGN DONG <kelvinson@foxmail.com>
2017-07-04 15:51:45 +0800

    add chartjs to visaulize the team and player's stats and unify the web's theme with light blue color

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -112,7 +112,15 @@ export   class App extends Component {
               </List>
               <Divider/>
             </div>
-            <div className="col s12 m5" >{this.showForm()}</div>
+
+          </div>
+          <div className="row">
+            <div className="col s12">
+            <br/>
+              <Divider/>
+              {this.showForm()}
+              <Divider/>
+            </div>
           </div>
         </div>
       </MuiThemeProvider>
--- server/methods.js
+++ server/methods.js
@@ -9,5 +9,10 @@ Meteor.methods({
   updatePlayer(player) {
     Players.update(player._id,
     {$set:player});
+  },
+
+  deletePlayer(playerId) {
+    Players.remove(playerId);
   }
+
 });
